fbe2b7f46e6811c17807fee94ae97ecd07d39e18
MOMO772824 <momo_772@icloud.com>
2026-08-14 21:17:16 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE="my-budget-v4";
+const ASSETS=["./","./index.html","./manifest.json"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch",e=>{
+ if(e.request.method!=="GET")return;
+ e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request).then(r=>{
+  const c=r.clone(); caches.open(CACHE).then(k=>k.put(e.request,c)); return r;
+ })));
+});
