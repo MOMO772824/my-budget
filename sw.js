737545b7fe3575587934e863101c1f9321ba400f
MOMO772824 <momo_772@icloud.com>
2026-08-14 21:29:52 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="my-budget-v4";
+const CACHE="my-budget-v5-delete";
 const ASSETS=["./","./index.html","./manifest.json"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
