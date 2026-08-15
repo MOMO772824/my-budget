a621e47a7d8cdf449719faa4ab8c9a97ee2c3f4c
MOMO772824 <momo_772@icloud.com>
2026-08-15 13:46:09 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,15 @@
-const CACHE="my-budget-v8-20260814";
-const ASSETS=["./","./index.html","./manifest.json"];
-self.addEventListener("install",e=>e.waitUntil((async()=>{await caches.open(CACHE).then(c=>c.addAll(ASSETS));self.skipWaiting();})()));
-self.addEventListener("activate",e=>e.waitUntil((async()=>{for(const k of await caches.keys()){if(k!==CACHE)await caches.delete(k)}await self.clients.claim();})()));
-self.addEventListener("fetch",e=>{
- if(e.request.method!=="GET")return;
- e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request).then(r=>{
-  const c=r.clone(); caches.open(CACHE).then(k=>k.put(e.request,c)); return r;
- })));
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", () => {
+  // キャッシュを使用しない
 });
