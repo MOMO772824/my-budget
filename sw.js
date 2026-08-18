f8bc01df8b4a358c25396d117b2bc4362a9a951b
MOMO772824 <momo_772@icloud.com>
2026-08-18 12:57:33 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,3 @@
-self.addEventListener("install", () => {
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(key => caches.delete(key)))
-    ).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener("fetch", () => {
-  // キャッシュを使用しない
-});
+self.addEventListener("install",()=>self.skipWaiting());
+self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",()=>{});
